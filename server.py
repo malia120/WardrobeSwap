b513f10c86520f56444a0ca90b8a690127fd556d
Maliha Ahmed <zkac334@live.rhul.ac.uk>
2023-10-14 11:44:13 +0100

    Fixed error where flask couldnt recognise folder

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder= "Frontend")
 
 @app.route('/')
 def home():
-    return "This is a tesT"
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug = True)
