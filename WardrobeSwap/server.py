2f47e164b763f659675a6ed3af6a3f931c474345
Maliha Ahmed <zkac334@live.rhul.ac.uk>
2023-10-15 15:34:05 +0100

    rearranged the files to look more organised

--- WardrobeSwap/server.py
+++ WardrobeSwap/server.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__, template_folder= "Frontend")
-db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+#db = SQLAlchemy(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
 @app.route('/')
 def home():
