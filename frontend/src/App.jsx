e175ec54e8a7350cb715bb02771c853d6b3df3d5
Maliha Ahmed <zkac334@live.rhul.ac.uk>
2023-11-13 15:57:04 +0000

    Fixed Navigation bar and implemented Search bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
 import {  Navbar  } from "./Components/Navbar";
+import { SearchBar } from "./Components/SearchBar";
 
 function App() {
   return (
     <React.Fragment> 
       <Navbar />
+      <div className="App"> 
+        <div className="search-bar-holder">
+          <SearchBar  />
+        </div>    
+      </div>
     </React.Fragment>
-  //   <div className='App'>
-  //     <h1>This is my frontend code</h1>
-  // </div>
   );
   
 }
