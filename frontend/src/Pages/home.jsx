f1df6ebc693eb3ab3d98170712acff65d202a408
Maliha Ahmed <zkac334@live.rhul.ac.uk>
2023-12-06 23:48:41 +0000

    trying to implement card

--- frontend/src/Pages/home.jsx
+++ frontend/src/Pages/home.jsx
@@ -2,18 +2,11 @@ import React, { useState, useEffect } from "react";
 import {  Navbar  } from "../Components/Navbar";
 import { SearchBar } from "../Components/SearchBar";
 import { Background } from "../Components/Background";
+import { Card } from "../Components/Card";
+
 const server = 'http://127.0.0.1:5000';
 
 function Home() {
-  const [initialData, setInitialData] = useState([]);
-
-  useEffect(()=>{
-    fetch(server + '/api').then(
-      response => response.json())
-    .then(data => setInitialData(data.listings))
-    .catch(error => console.error("Error fetching data:", error, error.message, error.stack))
-  }, []);
-
   return (
     <React.Fragment> 
     <Navbar />
@@ -23,18 +16,7 @@ function Home() {
       </div>  
       <Background />  
     </div>
-    <div className="App">
-      {initialData.map(item => (
-        <div key={item.id}>
-        <h1>{(item.title)}</h1>
-        <h2>{(item.Description)}</h2>
-        <h3>{(item.Category)}</h3>
-        <h4>{(item.Price)}</h4>
-        <h5>{(item.Image)}</h5>
-        <h6>{item.date_created}</h6>
-      </div>  
-      ))}
-    </div>
+    <Card />
     </React.Fragment> 
 
   );
