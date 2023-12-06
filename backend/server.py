c009dc01e3c8c29e8847adc50a1d79d5c2fe0a8e
Maliha Ahmed <zkac334@live.rhul.ac.uk>
2023-12-06 22:39:17 +0000

    created database

--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from datetime import datetime
 
 
 app = Flask(__name__) 
 CORS(app) 
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///listing.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db =SQLAlchemy(app)
+
+class Listing(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.String(10), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Title %r>' % self.id
 
 @app.route('/')
 def home():
@@ -11,11 +30,20 @@ def home():
 
 @app.route('/api', methods=['GET'])
 def api():
-    return ({
-        'userID': 1,
-        'title': 'Your clothes',
-        'completed': False
+    all_listing = Listing.query.all()
+    list_listing = []
+    for item in all_listing:
+       list_listing.append ({
+        'id': item.id,
+        'title': item.title,
+        'description': item.description,
+        'category': item.category,
+        'price': item.price,
+        'completed': item.completed,
     })
+    return {'listings': list_listing}
 
-if __name__ == "__main__": 
-    app.run(debug = True)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
