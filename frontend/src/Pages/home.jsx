af8695a4c815556b61ca8c389013cb3037ef7fe6
Maliha Ahmed <zkac334@live.rhul.ac.uk>
2023-12-06 23:35:28 +0000

    fixed home so it displays on home

--- frontend/src/Pages/home.jsx
+++ frontend/src/Pages/home.jsx
@@ -5,12 +5,12 @@ import { Background } from "../Components/Background";
 const server = 'http://127.0.0.1:5000';
 
 function Home() {
-  const [initialData, setInitialData] = useState({});
+  const [initialData, setInitialData] = useState([]);
 
   useEffect(()=>{
     fetch(server + '/api').then(
       response => response.json())
-    .then(data => setInitialData(data))
+    .then(data => setInitialData(data.listings))
     .catch(error => console.error("Error fetching data:", error, error.message, error.stack))
   }, []);
 
@@ -24,8 +24,17 @@ function Home() {
       <Background />  
     </div>
     <div className="App">
-        <h1>{(initialData.title + ' ' + initialData.completed)}</h1>
+      {initialData.map(item => (
+        <div key={item.id}>
+        <h1>{(item.title)}</h1>
+        <h2>{(item.Description)}</h2>
+        <h3>{(item.Category)}</h3>
+        <h4>{(item.Price)}</h4>
+        <h5>{(item.Image)}</h5>
+        <h6>{item.date_created}</h6>
       </div>  
+      ))}
+    </div>
     </React.Fragment> 
 
   );
