adbb479aa72bac881335900149925ff78212a7f7
zkac334 <maliha.ahmed.2021@live.rhul.ac.uk>
2024-04-10 05:26:49 +0100

    updated payment component

--- frontend/src/Components/Payment.jsx
+++ frontend/src/Components/Payment.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 export default function Payment() {
-  return (
-    <div>Payment</div>
-  )
+    const paypalButtons = useRef();
+
+    useEffect(() => {
+        if (!window.paypal) {
+            console.error("PayPal SDK not loaded.");
+            return;
+        }
+        
+        window.paypal.Buttons({
+            createOrder: function () {
 }
+        }).render(paypalButtons.current);
+            }, [onSuccess, onError]);
+
+            return (
+                <div>
+                    <div ref={paypalButtons}></div>
+                </div>
+            );
+};
+
